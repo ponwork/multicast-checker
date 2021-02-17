ec2b83a0b713e035e0ce25e22014ca658c93a6c1
ponwork <ponomarev.yuri@gmail.com>
2021-02-17 22:37:43 +0300

    Bug fix

--- multicast-checker.py
+++ multicast-checker.py
@@ -10,16 +10,18 @@ import smtplib
 import re
 import time
 import os
+from email.mime.text import MIMEText
 
 # Setup the command line argument parsing
 parser = argparse.ArgumentParser(description='Script to check the IPTV UDP streams from m3u playlist')
 
-parser.add_argument("--playlist",		help="Playlist *.m3u file with UDP streams",		required=True)
-parser.add_argument("--timeout",		help="Time to wait before checks in seconds",		default=600)
-parser.add_argument("--smtp_server",	help="SMTP server to send an email",				required=False)
-parser.add_argument("--smtp_port",		help="Port for SMTP server", 						default=25)
-parser.add_argument("--sender",			help="email address for email sender",				required=False)
-parser.add_argument("--recievers",		help="emails of the receivers (comma separated)",	required=False)
+parser.add_argument("--playlist",		help="Playlist *.m3u file with UDP streams",			required=True)
+parser.add_argument("--timeout",		help="Time to wait before checks in seconds",			default=600)
+parser.add_argument("--smtp_server",	help="SMTP server to send an email",					required=False)
+parser.add_argument("--smtp_port",		help="Port for SMTP server", 							default=25)
+parser.add_argument("--sender",			help="email address for email sender",					required=False)
+parser.add_argument("--receivers",		help="emails of the receivers (comma separated)",		required=False, action='append')
+parser.add_argument("--nic_ip",			help="network interface IP address with UDP stream",	default='', required=False)
 
 def playlist_parser(playlist):
 	""" Function that returns a dictionary of UDP streams"""
@@ -47,7 +49,7 @@ def playlist_parser(playlist):
 	
 	return dictionary
 
-def channel_checker(channel_address, channel_port):
+def channel_checker(channel_address, channel_port, nic_ip):
 	""" Function to check the given UDP stream """
 
 	# Creating the socket
@@ -64,7 +66,7 @@ def channel_checker(channel_address, channel_port):
 	# 1 representing a buffer
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	sock.bind(('', int(channel_port)))
+	sock.bind((nic_ip, int(channel_port)))
 
 	# Pack and format the socket data as following:
 	# '4sl' format: 4 - nember of bytes, s - char[] to bytes, l - long to integer
@@ -93,18 +95,18 @@ def channel_checker(channel_address, channel_port):
 def send_email(smtp_server, smtp_port, sender, receivers, channel_name, channel_addr, channel_port):
 	""" Function to send an email when IPTV channel failed to play"""
 
-	message = f'From: {sender}\
-	To: {receivers}\
-	Subject: IPTV stream issue for "{channel_name}" channel\n\
-	The channel "{channel_name}" is not working\n\
-	Address: {channel_addr}:{channel_port}'
+	msg = MIMEText(f'The channel "{channel_name}" is not working\nAddress: {channel_addr}:{channel_port}')
+	msg['Subject'] = f'IPTV stream issue for "{channel_name}" channel'
+	msg['From'] = f'{sender}'
+	msg['To'] = f'{receivers}'
 
 	try:
-	   smtpObj = smtplib.SMTP(smtp_server, smtp_port)
-	   smtpObj.sendmail(sender, receivers, message)         
-	   print(f'[*] An email has been sent')
+		smtpObj = smtplib.SMTP(smtp_server, smtp_port)
+		smtpObj.sendmail(sender, receivers, msg.as_string())
+		print(f'[*] An email has been sent')
 	except SMTPException:
-	   print(f'[*] Error: unable to send an email')
+		print(f'[*] Error: unable to send an email')
+
 
 # Define the script arguments as a <args> variable
 args = parser.parse_args()
@@ -122,12 +124,12 @@ while True:
 	# Check each channel in dictionary
 	for channel in channels_dictionary:
 		channel_address, channel_port = channels_dictionary[channel].split(':')
-		result = channel_checker(channel_address, channel_port)
+		result = channel_checker(channel_address, channel_port, args.nic_ip)
 		if result == 0:
 			print(f'[*] OK >>> Channel "{channel}" is working!')
 		else:
 			print(f'[*] !!! PROBLEM !!! Channel "{channel}" is not working!')
-			send_email(args.smtp_server, args.smtp_port, args.sender, args.receivers, channel_address, channel_port)
+			send_email(args.smtp_server, args.smtp_port, args.sender, args.receivers, channel, channel_address, channel_port)
 	
 	# Wait 10 minutes (default) to repeat
 	print(f'\nWaiting for {args.timeout} second(s) to repeat the check...\n')
